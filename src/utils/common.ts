f9fb6f1549e2b20d82afb33417486e5d965e5260
maitzeth <andre801mz@gmail.com>
2024-05-27 04:52:02 -0300

    refactor: :recycle: adding functions to common file

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -4,3 +4,19 @@ import { twMerge } from 'tailwind-merge';
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+export const extractEpisodesId = (episodes: string[] | undefined) => {
+  if (episodes && episodes.length > 0) {
+    return episodes.map((episode) => {
+      const id = episode.split('/').pop();
+  
+      return id as string;
+    });
+  }
+
+  return null;
+};
+
+export const getSharedElements = (array1: string[], array2: string[]) => {
+  return array1.filter(element => array2.includes(element));
+}
